fbe101a5cea782e95d02c52c8d108f61aecae91c
caroulin1704 <carod.emeraude@gmail.com>
2023-02-09 16:39:15 +0100

    Création de la connexion

--- toitplusmoifront/src/app/app.module.ts
+++ toitplusmoifront/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ConnexionComponent } from './connexion/connexion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConnexionComponent
   ],
   imports: [
     BrowserModule,

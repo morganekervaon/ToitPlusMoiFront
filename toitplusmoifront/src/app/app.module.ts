08bc2d8c3f73322580961e47415ca094c1dc313d
maelrikou <maelricouardfaou@gmail.com>
2023-02-09 16:48:15 +0100

    Creation inscription

--- toitplusmoifront/src/app/app.module.ts
+++ toitplusmoifront/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ConnexionComponent } from './connexion/connexion.component';
+import { InscriptionComponent } from './inscription/inscription.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ConnexionComponent
+    ConnexionComponent,
+    InscriptionComponent
   ],
   imports: [
     BrowserModule,

7b9d50b764cd77a569b10089c3e9882bb2c1ed20
morganekervaon <morganekervaon@gmail.com>
2023-02-13 14:08:29 +0100

    Ajout HttpClientModule

--- toitplusmoifront/src/app/app.module.ts
+++ toitplusmoifront/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ConnexionComponent } from './connexion/connexion.component';
@@ -20,7 +20,8 @@ import { AccueilComponent } from './accueil/accueil.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]

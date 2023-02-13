23cf477d1aeae41336028a375c7d99b6c28ea2e1
maelrikou <maelricouardfaou@gmail.com>
2023-02-13 12:08:39 +0100

    construction des routes vers les composants

--- toitplusmoifront/src/app/app-routing.module.ts
+++ toitplusmoifront/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccueilComponent } from './accueil/accueil.component';
+import { ConnexionComponent } from './connexion/connexion.component';
+import { InscriptionComponent } from './inscription/inscription.component';
+import { ProfilComponent } from './profil/profil.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'accueil', component: AccueilComponent },
+  { path: 'connexion', component: ConnexionComponent },
+  { path: 'inscritpion', component: InscriptionComponent },
+  { path: 'profil', component: ProfilComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

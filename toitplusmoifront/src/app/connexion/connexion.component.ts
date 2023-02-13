dc99ce0a21137ce8d2d6fa26c35574c4c24a4da4
maelrikou <maelricouardfaou@gmail.com>
2023-02-13 15:33:45 +0100

    connexionv3

--- toitplusmoifront/src/app/connexion/connexion.component.ts
+++ toitplusmoifront/src/app/connexion/connexion.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-connexion',
@@ -8,17 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConnexionComponent implements OnInit {
 
-  log: any;
-  constructor(private http: HttpClient) { }
+  private user: any;
+  constructor(private http: HttpClient, private route: Router) { }
 
   ngOnInit(): void {
 
   }
 
   connexion(val: any) {
-    this.http.get('http://localhost:8183/login').subscribe({
-      next: (data) => { this.log = data; },
-      error: (err) => { console.log(err) }
-    })
+    if (val.login !== '' && val.mdp !== '') {
+      this.http.post('http://localhost:8183/login', val).subscribe({
+        next: (data) => { this.user = data },
+        error: (err) => { console.log(err) }
+      })
+      console.log(val);
+      console.log(this.user);
+      if (!this.user === null) {
+        console.log("Connexion réussie");
+        this.route.navigateByUrl('accueil');
+      } else {
+        console.log("Connexion ratée");
+      }
+    } else {
+      console.log("remplis non ?");
+    }
   }
 }

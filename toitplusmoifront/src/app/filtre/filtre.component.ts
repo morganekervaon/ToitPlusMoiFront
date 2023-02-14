1ec6a00642c53cee4c38d17e3c329b24b0af63ed
morganekervaon <morganekervaon@gmail.com>
2023-02-14 10:31:54 +0100

    Redirection corrigée et filtre en HTML

--- toitplusmoifront/src/app/filtre/filtre.component.ts
+++ toitplusmoifront/src/app/filtre/filtre.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-filtre',
   templateUrl: './filtre.component.html',
   styleUrls: ['./filtre.component.css']
 })
-export class FiltreComponent {
+export class FiltreComponent implements OnInit {
 
+  constructor(private http: HttpClient, private route: Router) { }
+
+  ngOnInit(): void {
+
+  }
+
+  paramFiltre(val: any) {
+    let filtre;
+    console.log(filtre);
+    this.http.post('http://localhost:8183/filtre', filtre).subscribe({
+      next: (data) => {
+        console.log(data);
+        this.route.navigateByUrl('profil')
+      },
+      error: (err) => { console.log(err); }
+    })
+  }
 }

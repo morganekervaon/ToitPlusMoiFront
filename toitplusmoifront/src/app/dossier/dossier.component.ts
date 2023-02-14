6a19ba87292d49b311ead7e99340c217131c0980
caroulin1704 <carod.emeraude@gmail.com>
2023-02-14 17:14:27 +0100

    dossier et profil

--- toitplusmoifront/src/app/dossier/dossier.component.ts
+++ toitplusmoifront/src/app/dossier/dossier.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
+
 
 @Component({
   selector: 'app-dossier',
   templateUrl: './dossier.component.html',
   styleUrls: ['./dossier.component.css']
 })
-export class DossierComponent {
+export class DossierComponent implements OnInit {
+  constructor(private http: HttpClient, private route: Router, private authService : AuthService) {}
+  
+  ngOnInit(): void {
+  }
+  saveDossier(val: any){
+    this.http.post('http://localhost:8183/registerdossier', val).subscribe({
+      next: (data) => {
+        console.log(data);
+        this.route.navigateByUrl('profil')
+      },
+      error: (err) => { console.log(err); }
+    });
+  }
 
 }

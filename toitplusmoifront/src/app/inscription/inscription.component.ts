ce47ebbcde8469c04758d1a3f0cd66b3fabdcb0e
caroulin1704 <carod.emeraude@gmail.com>
2023-02-14 11:06:56 +0100

    finition inscription

--- toitplusmoifront/src/app/inscription/inscription.component.ts
+++ toitplusmoifront/src/app/inscription/inscription.component.ts
@@ -8,18 +8,21 @@ import { Router } from '@angular/router';
   styleUrls: ['./inscription.component.css']
 })
 export class InscriptionComponent implements OnInit {
-
+status: any;
   constructor(private http: HttpClient, private route : Router) {}
   ngOnInit(): void {
   }
 
   inscrireUser(val: any){
-    let user = { nom: val.nom, prenom: val.prenom, mail: val.mail, numero: val.numero, login: val.login, mdp: val.password};
-    console.log(user);
-    this.http.post('http://localhost:8183/inscription', user).subscribe({
+    //let user = { nom: val.nom, prenom: val.prenom, mail: val.mail, numero: val.numero, login: val.login, mdp: val.password};
+    console.log(val);
+    //if(this.status.equals("true")){
+    //  let user = { nom: val.nom, prenom: val.prenom, mail: val.mail, numero: val.numero, login: val.login, mdp: val.password, status: true};
+    //}
+    this.http.post('http://localhost:8183/inscription', val).subscribe({
       next: (data) => {
-        console.log(data);
-        this.route.navigateByUrl('accueil')
+       console.log(data);
+       this.route.navigateByUrl('accueil')
       },
       error:(err) => {console.log(err);}
     });

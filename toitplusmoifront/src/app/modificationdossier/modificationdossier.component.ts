ced9c9cf53ce3868e356aacd37e1df2f66de280d
caroulin1704 <carod.emeraude@gmail.com>
2023-02-15 15:49:29 +0100

    modifdoss avant dernière lol

--- toitplusmoifront/src/app/modificationdossier/modificationdossier.component.ts
+++ toitplusmoifront/src/app/modificationdossier/modificationdossier.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-modificationdossier',
   templateUrl: './modificationdossier.component.html',
   styleUrls: ['./modificationdossier.component.css']
 })
-export class ModificationdossierComponent {
+export class ModificationdossierComponent implements OnInit {
+  doss: any;
+  constructor(private http: HttpClient, private route: Router, private authService: AuthService) {}
+  public userConnect = this.authService.getUserConnect();
 
+  ngOnInit(): void{
+    this.http.get('http://localhost:8183/dossier/user/' + this.userConnect.id).subscribe({
+      next: (data) => {
+        this.doss=data;
+      },
+      error: (err) => {console.log(err);}
+    })
+  }
+  
+  modifDossier(doss: any) {
+    doss.locataire= this.userConnect;
+    this.http.put('http://localhost:8183/dossier/' + this.doss.id, doss).subscribe({
+      next: (data) => {
+        console.log(data);
+        this.route.navigateByUrl('profil')
+      },
+      error: (err) => { console.log(err)}
+    });
+    
+  }
 }

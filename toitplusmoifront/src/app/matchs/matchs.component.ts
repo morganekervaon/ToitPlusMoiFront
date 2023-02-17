1a1488ce1332c52d226eed00b5400373f04cb1ee
Auriax123 <auriane.orsati3@gmail.com>
2023-02-17 09:43:46 +0100

    sdfsdf

--- toitplusmoifront/src/app/matchs/matchs.component.ts
+++ toitplusmoifront/src/app/matchs/matchs.component.ts
@@ -1,4 +1,13 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
+
+
+
+
+
 
 @Component({
   selector: 'app-matchs',
@@ -7,4 +16,31 @@ import { Component } from '@angular/core';
 })
 export class MatchsComponent {
 
+
+  matchs: any;
+  constructor(private http: HttpClient, private route: Router, private authService: AuthService, public dialog: MatDialog) { }
+  public userConnect = this.authService.getUserConnect();
+  ngOnInit(): void {
+    if (this.userConnect.proprio) {
+      this.route.navigateByUrl('profilProprio');
+    } else {
+      this.route.navigateByUrl('profil');
+    }
+
+    this.http.get('http://localhost:8183/allMatchsAppart/' + this.userConnect.id).subscribe({
+      next: (data) => {
+        this.matchs = data;
+      },
+      error: (err) => { console.log(err); }
+    })
+  };
+  onNoClick(): void {
+    this.dialogRefModif.close();
+
+  }
 }
+
+
+
+
+
